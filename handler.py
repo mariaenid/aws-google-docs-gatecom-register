6c9bd0d1702a2f05f24096f8539d4cf8c1924c7e
Maria Pineda <mpineda@ioet.com>
2020-06-09 20:06:12 -0500

    Include fixes for apigoogle update

--- handler.py
+++ handler.py
@@ -1,44 +1,32 @@
-import os
-import json
 import datetime
+import json
+import os
 import pickle
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
-
-"""
-google docs link
-https://docs.google.com/spreadsheets/d/1V5Cq71O-XPWRqZ0lIsUYEj65LeVUQNHZFG5MhsLuVL0/edit?usp=sharing
-
-"""
 import pandas as pd
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 from numpy.random import randint
+from decouple import config
 
-columns = ['i','double','square']
-rows = []
+# change this by your sheet ID
+SAMPLE_SPREADSHEET_ID_input = config('SAMPLE_SPREADSHEET_ID_input')
+CREDENTIALS_JSON = config("CREDENTIALS_JSON")
 
-for i in range(6):
-    row = [i, i*2, i*i]
-    rows.append(row)
+# change the range if needed
+SAMPLE_RANGE_NAME = "A1:AA1000"
 
-df = pd.DataFrame(rows, columns=columns)
-
-#change this by your sheet ID
-SAMPLE_SPREADSHEET_ID_input = '1V5Cq71O-XPWRqZ0lIsUYEj65LeVUQNHZFG5MhsLuVL0'
-
-#change the range if needed
-SAMPLE_RANGE_NAME = 'A1:AA1000'
 
 def Create_Service(client_secret_file, api_service_name, api_version, *scopes):
     global service
     SCOPES = [scope for scope in scopes[0]]
-    #print(SCOPES)
+    # print(SCOPES)
 
     cred = None
 
-    if os.path.exists('token_write.pickle'):
-        with open('token_write.pickle', 'rb') as token:
+    if os.path.exists("token_write.pickle"):
+        with open("token_write.pickle", "rb") as token:
             cred = pickle.load(token)
 
     if not cred or not cred.valid:
@@ -48,47 +36,61 @@ def Create_Service(client_secret_file, api_service_name, api_version, *scopes):
             flow = InstalledAppFlow.from_client_secrets_file(client_secret_file, SCOPES)
             cred = flow.run_local_server()
 
-        with open('token_write.pickle', 'wb') as token:
+        with open("token_write.pickle", "wb") as token:
             pickle.dump(cred, token)
 
     try:
         service = build(api_service_name, api_version, credentials=cred)
-        print(api_service_name, 'service created successfully')
-        #return service
+        print(api_service_name, "service created successfully")
+        # return service
     except Exception as e:
         print(e)
-        #return None
+        # return None
+
 
 # change 'my_json_file.json' by your downloaded JSON file.
-Create_Service('credentials.json', 'sheets', 'v4',['https://www.googleapis.com/auth/spreadsheets'])
+Create_Service(
+    CREDENTIALS_JSON, "sheets", "v4", ["https://www.googleapis.com/auth/spreadsheets"]
+)
 
 
-def Export_Data_To_Sheets(gsheetId):
-    response_date = service.spreadsheets().values().append(
-        spreadsheetId=gsheetId,
-        valueInputOption='RAW',
-        range=SAMPLE_RANGE_NAME,
-        body=dict(
-            majorDimension='ROWS',
-            values=df.T.reset_index().T.values.tolist())
-    ).execute()
-    print('Sheet successfully Updated', response_date)
+def Export_Data_To_Sheets(payload):
+    """
+    google docs link
+    https://docs.google.com/spreadsheets/d/1V5Cq71O-XPWRqZ0lIsUYEj65LeVUQNHZFG5MhsLuVL0/edit?usp=sharing
+    """
+    # add args here
+    response_date = (
+        service.spreadsheets()
+        .values()
+        .append(
+            spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
+            valueInputOption="RAW",
+            range=SAMPLE_RANGE_NAME,
+            body=dict( majorDimension="ROWS", values=payload.T.reset_index().T.values.tolist()),
+        )
+        .execute()
+    )
+    print("Sheet successfully Updated", response_date)
+    return response_date
 
+
+def create_payload(user_id="000011982", date=1233475435, name="testing"):
+    row = (user_id, date, name,)
+
+    return pd.DataFrame([row])
 
 def endpoint(event, context):
     """
     Update googledocs
     """
     current_time = datetime.datetime.now().time()
-    Export_Data_To_Sheets(SAMPLE_SPREADSHEET_ID_input)
 
-    body = {
-        "message": "Hello, the current time is " + str(current_time)
-    }
+    payload_data = create_payload()
+    res = Export_Data_To_Sheets(payload_data)
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    body = {"message": "Hello, the current time is " + str(current_time) }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response

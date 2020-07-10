ffbba9e8bf5d2ceba0e0c0659c5d16ec30589ebc
Maria Pineda <mpineda@ioet.com>
2020-07-10 00:39:59 -0500

    Include fixes for save payload

--- handler.py
+++ handler.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import os
 import pickle
+import requests
 
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -16,6 +17,10 @@ CREDENTIALS_JSON = config("CREDENTIALS_JSON")
 # change the range if needed
 SAMPLE_RANGE_NAME = "A1:AA1000"
 
+# gatecom database
+USERNAME_GATECOM = config("USERNAME_GATECOM")
+PASSWORD_CATEGOM = config("PASSWORD_CATEGOM")
+URL_GATECOM = config("URL_GATECOM")
 
 def Create_Service(client_secret_file, api_service_name, api_version, *scopes):
     global service
@@ -74,32 +79,59 @@ def Export_Data_To_Sheets(payload):
     return response_date
 
 
-def create_payload(user_id="000011982", current_time=1233475435, name="Nina", device_id="Gatecom"):
-    row = [user_id, current_time, name, device_id]
+def create_payload(*args):
+    """
+    args**
+        user_id, current_time, name, device_id, state
+    """
+    return [[arg for arg in args]]
 
-    return [row]
+def get_user(user_id):
+    """
+    Response**
+        {'total_rows': 74,
+        'offset': 20,
+        'rows': [{'id': 'b788f2f01970b14249fe8506d84c114e',
+        'key': '30917B',
+        'value': {'name': 'Janina Herrera 2', 'estado': 'autorizado'}}]
+        }
+    """
+    client = requests.Session()
+    client.auth = (USERNAME_GATECOM, PASSWORD_CATEGOM)
 
+    url = "%s?key=\"%s\"" % (URL_GATECOM, user_id)
+    print("URL gatecommm", url)
+
+    r = client.get(url)
+    if r.status_code == 200 and len(r.json()["rows"]) == 1:
+        return r.json()["rows"][0].get("value", None) # the first record
+    else:
+        raise "Can't Find key %s" % user_id
 
 def endpoint(event, context):
     """
     Update googledocs
     """
     user_id = event.get("user_id", None)
-    name = event.get("name", None)
     device_id = event.get("device_id", None)
-    current_time = str(datetime.datetime.now().time())
+    current_time = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
 
-    if user_id and name:
+    if user_id:
         print("Updating access with id {} on {}".format(user_id, device_id))
 
-        add_args = lambda **k: k
-        payload = add_args(user_id=user_id, name=name, device_id=device_id, current_time=current_time)
-        payload_data = create_payload(**payload)
+        try:
+            user_gatecom = get_user(user_id)
+            name = user_gatecom.get("name", None)
+            estado = user_gatecom.get("estado", None)
 
-        res = Export_Data_To_Sheets(payload_data)
-        status = 200
-    else:
-        status = 404
+            # add_args = lambda *k: k
+            payload_data = create_payload(user_id, name, device_id, current_time, estado)
+
+            res = Export_Data_To_Sheets(payload_data)
+            status = 200
+        except Exception as e:
+            print("Error %s" % e)
+            status = 400
 
     body = {"message": "Hello, the current time is " + str(current_time)}
 
